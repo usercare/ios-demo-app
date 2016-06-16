8106e0829ea006d09a36abbc945380464f53bba5
Kirill Kazachenko <h32.android@gmail.com>
2016-06-16 13:26:09 +0300

    Migrate to pods and UC SDK 1.0.8

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'UserCareDemo' do
+  pod 'UserCareSDK', '~> 1.0.8'
+end
--- UserCareDemo/UserCareSDK.framework/Headers/UCActionEntity.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface UCActionEntity : NSObject
-
-@property (nonatomic, copy) NSString *actionTitle;
-@property (nonatomic, copy) NSString *actionImageUrl;
-
-@end
